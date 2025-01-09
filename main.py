19179fe761281a923b96ee2a4d318a97ade207f4
liuyp2080 <78184640+liuyp2080@users.noreply.github.com>
2025-01-09 19:20:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import uvicorn
 import os
 import pandas as pd
 import numpy as np
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 # load_dotenv()
 # root_path = os.getenv("SCRIPT_NAME", "")
